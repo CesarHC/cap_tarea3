d5f102bcccbce6bdf5f6c6e905678289fb4dfac1
cesarphc <cesarherreracriso@gmail.com>
2022-11-28 11:47:26 -0300

    Pagina principal

--- actividad3/urls.py
+++ actividad3/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from principal.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index)
 ]
