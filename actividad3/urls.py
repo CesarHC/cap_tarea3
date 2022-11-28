9fc9ff21e563ebcc9b634164691c57b24689e50f
cesarphc <cesarherreracriso@gmail.com>
2022-11-28 12:01:50 -0300

    Creacion modulo terciario

--- actividad3/urls.py
+++ actividad3/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from principal.views import index
+from terciaria.views import terciaria
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('terciaria/', terciaria),
 ]
